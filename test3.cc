23c73605e5014ffdaf70fb04ffc2780f9bbf5d6c
Ayres Freitas <97195880+ayresfreitas@users.noreply.github.com>
2022-02-14 12:42:48 -0500

    Update test3.cc

--- test3.cc
+++ test3.cc
@@ -1,1 +1,69 @@
+#include <iostream>
+using namespace std;
 
+#include "EWPOZ.h"
+#include "xscnnlo.h"
+#include "SMval.h"
+
+int main()
+{
+  SMval myinput;	// convert masses from PDG values to complex pole scheme
+  myinput.set(al, 1/137.03599976);
+  myinput.set(MZ, 91.1876);
+  myinput.set(MW, 80.4044);
+  myinput.set(GamZ, 2.4952);
+  myinput.set(GamW, 2.115);
+  myinput.set(MH, 125.1);
+  myinput.set(MT, 172.5);
+  myinput.set(MB, 0);
+  myinput.set(Delal, 0.059);
+  
+  cout << endl << "Complex-pole masses: MW=" << myinput.get(MWc) << ", MZ=" 
+    << myinput.get(MZc) << endl << endl;
+ 
+  // compute matrix element for ee->bb with axial-vector coupling in initial
+  // state and vector coupling in final state
+  int ini = LEP, fin = BQU, iff = AXV, off = VEC;
+  string typenam[5] = {"l", "n", "u", "d", "b"};
+  string formnam[2] = {"V", "A"};
+  
+  cout << "i = " << typenam[ini] << ", f = " << typenam[fin] <<
+    " ========================================" << endl << endl;
+  
+  // compute vertex form factors:
+  FA_SMNLO FAe(ini, myinput), FAb(fin, myinput);
+  SW_SMNLO SWe(ini, myinput), SWb(fin, myinput);
+  cout << "F_A^i (NLO) = " << FAe.result() << endl;
+  cout << "F_A^f (NLO) = " << FAb.result() << endl;
+  cout << "sineff^i (NLO) = " << SWe.result() << endl;
+  cout << "sineff^f (NLO) = " << SWb.result() << endl;
+  cout << endl;
+  
+  double cme = 88.,  // center-of-mass energy
+         cost = 0.5; // scattering angle
+
+  // compute matrix element for ee->ff using SM form factors:
+  R_SMNNLO R(ini, fin, iff, off, FAe, FAb, SWe, SWb, myinput, cost);
+  S_SMNLO S(ini, fin, iff, off, myinput, cost);
+  Sp_SMLO Sp(ini, fin, iff, off, myinput);
+  cout << "R_" << formnam[iff] << formnam[off] << " = " << R.result() << endl;
+  cout << "S_" << formnam[iff] << formnam[off] << " = " << S.result() << endl;
+  cout << "S'_" << formnam[iff] << formnam[off] << "= " << Sp.result() << endl;
+  
+  double mz = myinput.get(MZ), gz = myinput.get(GamZ);
+  Cplx sminuss0 = cme*cme - Cplx(mz*mz,-mz*gz);
+  Cplx matel1 = R.result()/sminuss0 + S.result() + Sp.result()*sminuss0;
+  cout << "SM matrix element M_" << formnam[iff] << formnam[off] << " = " << matel1 << endl;
+  cout << " ( \" ) squared |M_" << formnam[iff] << formnam[off] << "|^2 = " << sqr(abs(matel1)) << endl;
+  
+  // directly compute squared matrix element for ee->bb using SM form factors:
+  msq_SMNNLO Msq(ini, fin, iff, off, iff, off, FAe, FAb, SWe, SWb, 
+  		 cme*cme, cost, myinput);
+  cout << "Direct computation of SM matrix element squared |M_" << formnam[iff] << formnam[off] << "|^2 = " 
+  	<< Msq.result() << endl;
+  cout << endl;
+ }
+ }
+  
+  return 0;
+}
